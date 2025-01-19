3dfaf091948920332853ea64c1c6dba0128f59f0
painfultrait <128262634+painfultrait@users.noreply.github.com>
2025-01-19 06:59:33 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+const button = document.getElementById("menu");
+const menu = document.getElementById("nav-menu");
+button.addEventListener('click', toggleActive);
+function toggleActive() {
+  button.classList.toggle('active');
+  menu.classList.toggle('active');
+  
+}
